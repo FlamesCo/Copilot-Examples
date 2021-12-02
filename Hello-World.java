b80185539d5df6b29bda3ab73510405ca155e5c8
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2021-12-01 18:54:25 -0800

    Hello World 1.0 J
    hello world!

---
+++ Hello-World.java
@@ -0,0 +1,9 @@
+// write hello world in java
+public class HelloWorld { 
+    public static void main(String[] args) {
+// fix the intended 
+        System.out.println("Hello World"); } {
+
+        }
+    
+    }
